2aa5bd034c4b4853bcfa37ddcf35b9b261f47c47
benedekaibas <kaibas.benedek02@gmail.com>
2025-02-12 00:57:05 -0500

    finished checked c benchmark code

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Fat-Pointers-Benchmarking
+
 Doing Benchmarking Experiments Using Fat Pointers in C and Checked C
+
+## How To Compile the files
+
+### Default C
+
+- cd to the path of your folder where you cloned the repository
+
+`cd path/to/your/source/directory`
+
+- Compile the c program file using GCC 14
+
+`/usr/local/bin/gcc-14 -O3 default_c.c -fopenmp -o default-c`
+
+- Run the compiled program with the following command
+
+`time ./default_c`
+
+### Checked C
---
+++ src/checked_c.c
@@ -0,0 +1,69 @@
+#include <stdlib_checked.h>
+#include <string_checked.h>
+#include <stdio_checked.h>
+#include <time_checked.h>
+#include <omp.h>
+
+#define N 20000
+#define THREADS_NB omp_get_max_threads()
+
+void init_arrays(_Array_ptr<double> a : count(N), _Array_ptr<double> b : count(N)) {
+  memset(a, 0, N * sizeof(double));
+  memset(b, 0, N * sizeof(double));
+  for (int i = 0; i < N; i++) {
+    a[i] += 1.0;
+    b[i] += 1.0;
+  }
+}
+
+double func2(double i, double j) {
+  double res = 0.0;
+
+  while (i / j > 0.0) {
+    res += i / j;
+    i -= 0.1;
+    j -= 0.000003;
+  }
+  return res;
+}
+
+double single_thread(_Array_ptr<double> a : count(N), _Array_ptr<double> b : count(N)) {
+  double res = 0;
+  int i, j;
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      if (i == j) continue;
+      res += func2(a[i], b[j]);
+    }
+  }
+  return res;
+}
+
+double multi_threads(_Array_ptr<double> a : count(N), _Array_ptr<double> b : count(N)) {
+  double res = 0;
+  int i, j;
+  #pragma omp parallel for private(j) num_threads(THREADS_NB) reduction(+:res)
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++) {
+      if (i == j) continue;
+      res += func2(a[i], b[j]);
+    }
+  }
+  return res;
+}
+
+int main(void) {
+  _Array_ptr<double> a : count(N) = calloc<double>(N, sizeof(double));
+  _Array_ptr<double> b : count(N) = calloc<double>(N, sizeof(double));
+  init_arrays(a, b);
+
+  clock_t start_time = clock();
+  double res = single_thread(a, b);
+  double elapsed_time = (double)(clock() - start_time) / CLOCKS_PER_SEC;
+  printf("Default:  Done with %f in %f sd\n", res, elapsed_time);
+
+  start_time = clock();
+  res = multi_threads(a, b);
+  elapsed_time = (double)(clock() - start_time) / CLOCKS_PER_SEC;
+  printf("With OMP: Done with %f in %f sd\n", res, elapsed_time / THREADS_NB);
+}
